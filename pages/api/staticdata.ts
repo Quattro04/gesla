32097eb1266c1e0664db6b94a19a0bccb3176372
Matija Jeras <matija.jeras64@gmail.com>
2023-01-29 11:27:55 +0100

    fix staticdata

--- pages/api/staticdata.ts
+++ pages/api/staticdata.ts
@@ -1,6 +1,6 @@
 import { promises as fs } from 'fs';
 
 export default async function handler(req: any, res: any) {
-  const fileContents = await fs.readFile('data.txt', 'utf8');
+  const fileContents = await fs.readFile('data', 'utf8');
   res.status(200).json(fileContents);
 }
